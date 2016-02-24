f113589760abb1cbf87500cc2d43c5260d7742d2
王某槑 <1628283919@qq.com>
2016-02-24 13:28:11 +0800

    新环境搭建

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,6 +33,9 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/sweetalert/dist/sweetalert.min.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/bootstrap-switch/dist/js/bootstrap-switch.js',
+      'bower_components/angular-bootstrap-switch/dist/angular-bootstrap-switch.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
