aec2fc7a133a88fff8174441bbebcc7afa86dd65
Chenghongchao <ajlinger@163.com>
2016-07-01 09:44:31 +0800

    接入相互调宿

--- app/scripts/controllers/flatManager/hutiao.js
+++ app/scripts/controllers/flatManager/hutiao.js
@@ -8,16 +8,19 @@
  * Controller of the flatpcApp
  */
 angular.module('flatpcApp')
-.controller('hutiaoCtrl', ['$scope','$rootScope',function($scope,$rootScope) {
+.controller('hutiaoCtrl', ['$scope','$rootScope','AppConfig',function($scope,$rootScope,AppConfig) {
         //存储列表头到frame.html中
     $scope.menus = [
         '公寓管理','日常调换宿','相互调宿'
     ];
-    //跳转到什么地方去
+     //跳转到什么地方去
     $scope.parent = "flat";
-    $rootScope.loading = false;
+    $scope.loaded = function(){
+        $rootScope.loading = false;
+        $scope.$apply();
+    }
     var a = document.createElement('a');
-    a.href = "http://baidu.com";
+    a.href = AppConfig.FRAME + "index.php?m=Apartment&c=Between&a=index&token="+AppConfig.token+"&schoolcode="+AppConfig.schoolCode;
     a.target="page-frame";
     a.click();
 }]);
