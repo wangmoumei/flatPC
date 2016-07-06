5bcff0228dce2cc7e57adcd0886432a750d241d8
Chenghongchao <ajlinger@163.com>
2016-07-06 14:35:30 +0800

    更新

--- app/scripts/controllers/collect/reportway.js
+++ app/scripts/controllers/collect/reportway.js
@@ -13,7 +13,7 @@ angular.module('flatpcApp')
         $scope.$apply();
     }
     var a = document.createElement('a');
-    a.href = AppConfig.REPORT + "index.php?m=Admin&c=Config&a=statistics&schoolid="+AppConfig.token+"&schoolcode="+AppConfig.schoolCode;
+    a.href = AppConfig.REPORT + "index.php?m=Admin&c=Config&a=statistics&schoolid="+AppConfig.schoolCode;
     a.target="page-frame";
     a.click();
 }]);
