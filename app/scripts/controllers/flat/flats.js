454eb19849d387fa6007a3df5d3c4130a9069511
Chenghongchao <ajlinger@163.com>
2016-07-08 09:47:46 +0800

    更新

--- app/scripts/controllers/flat/flats.js
+++ app/scripts/controllers/flat/flats.js
@@ -2,7 +2,7 @@ angular.module('flatpcApp')
 .controller('FlatCtrls', ['$scope','$rootScope','AppConfig',function($scope,$rootScope,AppConfig) {
         //存储列表头到frame.html中
     $scope.menus = [
-        '公寓管理','综合查询','按楼查看'
+        '公寓管理','公寓管理','按楼查看'
     ];
     //跳转到什么地方去
     $scope.parent = "flat";
